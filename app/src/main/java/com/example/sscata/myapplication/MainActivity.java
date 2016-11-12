213a499e131f86b849857e17b92ab332c6496d97
Sascha <sscata@unibz.it>
2016-11-12 15:35:16 +0100

    Vibration added

---
+++ app/src/main/java/com/example/sscata/myapplication/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.sscata.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import android.widget.Button;
+
+import static android.R.attr.value;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox vibration, gputest, rendering;
+    RadioButton duration;
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+                //Toast.makeText(MainActivity.this, cat, Toast.LENGTH_SHORT).show();
+                //else
+                //{
+                //    Toast.makeText(MainActivity.this,"First checkbox Unchecked", Toast.LENGTH_SHORT).show();
+                //}
+                //Intent myIntent = new Intent(MainActivity.this, MyGLActivity.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                //MainActivity.this.startActivity(myIntent);
+    }
+
+    public void startTest(View view){
+<<<<<<< HEAD
+        //Intent intent = new Intent(this, MyGLActivity.class);
+        //startActivity(intent);
+
+        Thread thread = new Thread( new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    Intent intent = new Intent(MainActivity.this, MyGLActivity.class);
+                    startActivity(intent);
+                    Thread.sleep(5000);
+                    Intent intent2 = new Intent(MainActivity.this, VibrationActivity.class);
+                    startActivity(intent2);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                //finish();
+            }
+        }
+    });
+
+    thread.start();
+=======
+        Intent intent = new Intent(this, MyGLActivity.class);
+        startActivity(intent);
+>>>>>>> e38a6178cdaeb2924cd810ee159a7e3d5ac6576b
+    }
+
+}
